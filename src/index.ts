aba0113e673ec799ee382af631fa71dfaad5dee2
Gavin Bauman <gavinbauman@Gavins-MBP.attlocal.net>
2018-09-23 17:59:49 -0400

    Can talk to blob storage but finding file fails

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import express from 'express';
+import * as csvRouter from './controllers/csvRouter'
+
+
 
 // Creates a new Express app instance
 const app = express();
 
 // Configures the http://localhost:5000/ route to send a text response
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
+app.get('/', (req, res, next) => {
+    csvRouter.getCSV(req, res)
+        .then((result) =>{
+            res.send(result) 
+        }).catch((err) => {
+            res.send(`Error: ${err}`)
+        })
 });
+
 
 // Starts the app on port 5000, then calls the callback when 
 // the app successfully starts.
