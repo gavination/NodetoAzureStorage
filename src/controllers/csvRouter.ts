539f047c1a26a7c943ca5db6a498fdbe2aa8420b
Gavin Bauman <gavinbauman1322@gmail.com>
2018-09-24 09:55:10 -0400

    Can download sample CSV to file

--- src/controllers/csvRouter.ts
+++ src/controllers/csvRouter.ts
@@ -1,28 +1,30 @@
 // Reference the Request and Response types from express
 import { Request, Response } from 'express';
 import * as Storage from 'azure-storage';
-import  * as path from 'path';
-import * as dotenv from 'dotenv'
+import * as path from 'path';
+import * as dotenv from 'dotenv';
+import * as fs from 'fs';
 
 dotenv.config();
 
 let Token = process.env.AZURE_STORAGE_CONNECTION_STRING;
 let BlobService = Storage.createBlobService(Token);
-let ContainerName = 'sample-container';
+let ContainerName = 'testcontainer';
 let DownloadFilepath = path.resolve('./samplebatchfile.csv');
-let BlobName = "samplebatchfile"
+let BlobName = "samplebatchfile.csv"
+
 
 
 export function getCSV(req: Request, res: Response) {
     return new Promise((resolve, reject) => {
-        BlobService.getBlobToLocalFile(ContainerName, BlobName, DownloadFilepath, err => {
+           BlobService.getBlobToStream(ContainerName, BlobName, fs.createWriteStream(DownloadFilepath), err => {
             if(err) {
                 reject(err);
             } else {
                 resolve({ message: `Download of '${BlobName}' complete`});
             }
+           })           
         })
-    })
 }
 
 
